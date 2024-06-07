24ba5fdbbb1bc06d6678119b573f1350f0a0ad17
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-06-07 18:10:09 +0200

    add hw35

---
+++ script.js
@@ -0,0 +1,25 @@
+const productList = document.getElementById('container')
+console.log(productList);
+async function fetchQuery(){
+    const res = await fetch('https://dummyjson.com/products')
+    const data = await res.json()
+
+    data.products.map(product => {
+        const productCard = document.createElement('div')
+        productCard.classList.add('productCart')
+        const cardTitle = document.createElement('h2')
+        cardTitle.textContent = product.title;
+
+        const cardImage = document.createElement('img')
+        cardImage.src = product.images[0] 
+        cardImage.classList.add('cardImage')
+
+        const cardDescription = document.createElement('h4')
+        cardDescription.textContent = product.description
+         
+        
+        productCard.append(cardTitle, cardImage, cardDescription)
+        productList.append(productCard)
+    })
+}
+fetchQuery()
