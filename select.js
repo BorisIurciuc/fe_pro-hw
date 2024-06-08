432c559fa302d7c962c6ae5e48a8c251fd03445f
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-06-09 00:52:10 +0200

    add select.js

---
+++ select.js
@@ -0,0 +1,28 @@
+const productList = document.querySelector('#container')
+
+async function fetchQuery(){
+    const res = await fetch('https://dummyjson.com/products')
+    const data = await res.json()
+
+    data.products.map(product => {
+        const productCard = document.createElement("div")
+        productCard.classList.add('productCart')
+
+        const cardTitle = document.createElement('h3')
+        cardTitle.textContent = product.title;
+
+        const cardImage = document.createElement('img')
+        cardImage.src = product.images[0]
+        cardImage.classList.add('cardImage')
+
+        const cardDescription = document.createElement('p')
+        cardDescription.textContent = product.description;
+
+
+        productCard.append(cardTitle, cardImage, cardDescription)
+        productList.append(productCard)
+
+    })
+}
+
+fetchQuery()
